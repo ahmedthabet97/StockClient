94af2aabed95769640163142d4472262b3abfbb1
ahmedthabet97 <ahmedaboaouf333@gmail.com>
2024-02-01 16:16:15 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavMenuComponent]
 })
 export class AppComponent {
   title = 'StockClient';
