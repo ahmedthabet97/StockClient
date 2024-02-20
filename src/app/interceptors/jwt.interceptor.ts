856aa1de73ad61c1e5f874df6509e89875afa787
ahmedthabet97 <ahmedaboaouf333@gmail.com>
2024-02-20 16:46:05 +0200

    jwtInterceptor

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,6 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { User } from '../Models/User';
+import { request } from 'express';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log(req.url)
+ 
+
+  if (typeof window !== 'undefined') {
+    const key = localStorage.getItem(environment.userKey);
+    if (key) {
+      const user: User = JSON.parse(key);
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${user.jwt}`
+        }
+      });
+    }         
+  }
   return next(req);
 };
